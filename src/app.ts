30a18887559d80218554d53404f6748b568cf305
codeBelt <code@codebelt.com>
2016-01-11 23:47:10 -0600

    initial

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@
 import * as hapi from "hapi";
 import Controllers from "./controllers";
 import * as fs from "fs";
+import * as fsp from "fs-promise";
 import * as mkdirp from "mkdirp";
+import * as nunjucks from "nunjucks";
+
 
 var port = process.env.port || 3000;
 var server = new hapi.Server();
@@ -15,15 +18,56 @@ server.connection({ port: port });
 Controllers(server);
 
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
 
 
-mkdirp('tmp/some/path/foo', function(err) {
+//http://stackoverflow.com/questions/10049557/reading-all-files-in-a-directory-store-them-in-objects-and-send-the-object
+const htmlData = {
+  title : 'My First Nunjucks Page',
+  items : [
+    { name : 'item #1' },
+    { name : 'item #2' },
+    { name : 'item #3' },
+    { name : 'item #4' },
+  ]
+}
 
+const html =
+`<!doctype html>
+<html>
+  <head>
+  <title>welcome to {{ title }}</title>
+  </head>
+  <body>
+    <ul>
+    {% for item in items %}
+      <li>{{ item.name }}</li>
+    {% endfor %}
+    </ul>
+  </body>
+</html>`;
+
+var template = nunjucks.compile(html);
+const rendered = template.render(htmlData);
+
+
+fsp.writeFile('hello1.txt', 'hello world')
+  .then(() => {
+    return fsp.readFile('hello1.txt', {encoding:'utf8'});
+  })
+  .then((contents) => {
+    console.log("contentss", contents);
+  });
+
+mkdirp('tmp/some/path/foo', function(err) {
   // path was created unless there was error
-  fs.writeFile('tmp/some/path/foo/ddd.txt', 'Hello Node.js', (err) => {
-    if (err) throw err;
+  fs.writeFile('tmp/some/path/foo/ddd.html', rendered, (err) => {
+    if (err) {
+      throw err;
+    }
+
+
     console.log('It\'s saved!');
   });
 });
